6079b25ce64ee1d151e4d38173b325cd0827301e
user <joj8471@gmail.com>
2023-04-07 08:45:42 +0900

    test

---
+++ content/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from .models import Feed
+
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id')
+
+
+        return render(request, 'star/main.html', context=dict(feeds=feed_list))
