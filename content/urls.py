b962261100f4c847d6959371be60e03bd814f227
user <joj8471@gmail.com>
2023-04-20 17:31:22 +0900

    2023-04-11

--- content/urls.py
+++ content/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import UploadFeed
+from .views import UploadFeed,Profile
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('upload', UploadFeed.as_view())
+    path('upload', UploadFeed.as_view()),
+    path('profile', Profile.as_view())
 ]
